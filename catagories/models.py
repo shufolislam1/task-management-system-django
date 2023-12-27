f9f64fda2d16c892369c9c2744c8699bdcd4713b
shufolislam1 <shufolislam1255@gmail.com>
2023-12-28 00:28:21 +0600

    some error solved

--- catagories/models.py
+++ catagories/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from task.models import taskModel
+# from task.models import taskModel
 
 # Create your models here.
 class catagoryModel(models.Model):
     catagoryName = models.CharField(max_length = 20)
-    task = models.ManyToManyField(taskModel, default=None)
+    task = models.CharField(max_length = 250, default=None)
     
     def __str__(self):
         return self.catagoryName
--- task/models.py
+++ task/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from catagories.models import catagoryModel
 
 # Create your models here.
 
 class taskModel(models.Model):
     taskTitle = models.CharField(max_length=20)
     taskDescription = models.TextField()
+    taskCatagories = models.ManyToManyField(catagoryModel, default=None)
     is_completed = models.BooleanField(default=False)
     taskAssignedDate = models.DateTimeField(auto_now_add=True)
     
