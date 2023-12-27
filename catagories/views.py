7fcde149e06871f05a5b93647a464f58fcf59e27
shufolislam1 <shufolislam1255@gmail.com>
2023-12-28 00:57:05 +0600

    save done

--- catagories/views.py
+++ catagories/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import catagory_model_form
 
 # Create your views here.
 def add_catagory(request):
-    addCatagory = catagory_model_form()
+    if request.method =='POST':
+        addCatagory = catagory_model_form(request.POST)
+        if addCatagory.is_valid():
+            addCatagory.save()
+            return redirect('addCatagory')
+        
+    else:
+        addCatagory = catagory_model_form()
     return render(request, 'add_catagory.html', {'data': addCatagory})
 
--- task/views.py
+++ task/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import task_Model_Form
 
 # Create your views here.
 def add_task(request):
-    task_form = task_Model_Form()
+    if request.method == 'POST':
+        task_form = task_Model_Form(request.POST)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('addTask')
+        
+    else:
+        task_form = task_Model_Form()
     return render(request, 'add_task.html', {'data':task_form})
     
