f9f17abf479d74186b18a20ca23bf2d4dcbf80ae
shufolislam1 <shufolislam1255@gmail.com>
2024-01-11 22:11:34 +0600

    delete edit done

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import add_task
+from .views import add_task, edit_task, delete_task
 
 urlpatterns = [
-    path('add/', add_task, name='addTask')
+    path('add/', add_task, name='addTask'),
+    path('edit/<int:id>',edit_task ,name='editTask'),
+    path('delete/<int:id>',delete_task ,name='deleteTask'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import task_Model_Form
+from .models import taskModel
 
 # Create your views here.
 def add_task(request):
@@ -14,3 +15,17 @@ def add_task(request):
     print(task_form)
     return render(request, 'add_task.html', {'data':task_form})
     
+def edit_task(request, id):
+    task = taskModel.objects.get(pk=id)
+    task_form = task_Model_Form(instance=task)
+    if request.method == 'POST':
+        task_form = task_Model_Form(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('homepage')
+    return render(request, 'add_task.html', {'data':task_form})
+
+def delete_task(request, id):
+    task = taskModel.objects.get(pk=id)
+    task.delete()
+    return redirect('homepage')
