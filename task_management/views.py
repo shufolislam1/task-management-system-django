50564a9852b4716ae928f0946b75b13758517ace
shufolislam1 <shufolislam1255@gmail.com>
2024-01-11 17:53:08 +0600

    debbuging done

--- task_management/views.py
+++ task_management/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from task.models import taskModel
+from catagories.models import catagoryModel
 
 def showTasks(request):
     data = taskModel.objects.all()
-    print(data)
-    return render(request, 'show_tasks.html', {'data':data})
+    cataData = catagoryModel.objects.all()
+    # print(cataData)
+    return render(request, 'show_tasks.html', {'data':data, 'cataData':cataData})
